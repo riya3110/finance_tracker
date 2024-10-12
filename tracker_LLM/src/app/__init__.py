de9997c561a991636a3daa1164e3034b177a02d1
RIYA <RIYA@gmail.com>
2024-10-12 06:30:59 -0700

    commit

---
+++ tracker_LLM/src/app/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import request, jsonify
+from services.messageService import MessageService
+from kafka import KafkaProducer
+import json
+
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+messageService = MessageService()
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+@app.route('/v1/ds/message', methods=['POST'])
+def handle_message():
+    message = request.json.get('message')
+    result = messageService.process_message(message)
+    serialized_result = result.json()
+
+    producer.send('expense_service', serialized_result)
+
+    return jsonify(result)
+
+@app.route('/', methods=['GET'])
+def handle_get():
+    return 'Hello world'
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8080, debug=True)
---
+++ tracker_LLM/src/app/services/messageService.py
@@ -0,0 +1,16 @@
+from utils.messageUtil import MessageUtil
+from services.llmService import LLMService
+
+
+class MessageService:
+    def __init__(self):
+        self.messageUtil = MessageUtil()
+        self.llmService = LLMService()
+
+    def process_message(self , message):
+        if self.messageUtil.isBankSms(message):
+            return self.llmService.runLLM(message)
+        else:
+            return None
+
+
